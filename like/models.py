04de590cb9351f17d9d5ad75a7736e851d796457
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-01-01 00:22:54 +0330

    create: models in like.models

--- like/models.py
+++ like/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 # Create your models here.
+
+class LikeItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='کاربر')
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.UUIDField()
+    content_object = GenericForeignKey()
+
+    class Meta:
+        verbose_name_plural = 'پسندیده شده توسط کاربر'
