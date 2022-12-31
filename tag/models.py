21b7e3da93126bc045efe34aeca9a7fb71a680a2
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-01-01 00:11:44 +0330

    create: models in tag.models

--- tag/models.py
+++ tag/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 # Create your models here.
+
+
+class Tag(models.Model):
+    label = models.CharField(max_length=255, verbose_name='عنوان برچسب')
+
+    class Meta:
+        verbose_name_plural = 'برچسب'
+        ordering = ['label']
+
+
+class TagItem(models.Model):
+    tag = models.ForeignKey(
+        'Tag', on_delete=models.CASCADE, verbose_name='برچسب')
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.UUIDField()
+    content_object = GenericForeignKey()
+
+    class Meta:
+        verbose_name_plural = 'برچسب انتخابی توسط کاربر'
