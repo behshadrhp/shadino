e639e653827208b06d7871aa1da58366ab093553
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-01-03 12:21:49 +0330

    change: admin panel description

--- core/urls.py
+++ core/urls.py
@@ -5,3 +5,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('store/', include('store.urls')),
 ]
+
+
+admin.site.site_header = 'شادینو'
+admin.site.site_title = 'پنل مدیریتی فروشگاه شادینو'
+admin.site.index = 'فروشگاه شادینو'
