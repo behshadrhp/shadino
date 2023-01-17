fe22d9ae7fb56274883dcfafb9ea873b12232e9f
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-01-18 00:22:29 +0330

    update: ProductSerializer

--- store/serializers.py
+++ store/serializers.py
@@ -1,12 +1,16 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from .models import Product
 
 
 class ProductSerializer(ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id',  'title', 'price', 'updated']
+        fields = ['id',  'title', 'price', 'collection', 'updated']
 
+    collection = SerializerMethodField()
+
+    def get_collection(self, product: Product):
+        return product.collection.title
 
 class ProductCreateUpdateSerializer(ModelSerializer):
     class Meta:
