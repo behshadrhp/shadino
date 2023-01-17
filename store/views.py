091e6e3ec527931f64ede62e44813e6fa88f06bd
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-01-17 18:14:51 +0330

    update: views.ProductViewSet - create destory for delete product

--- store/views.py
+++ store/views.py
@@ -1,4 +1,8 @@
+from django.shortcuts import get_object_or_404
+from django.db.models import ProtectedError
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework import status
 from .models import Product
 from .serializers import ProductSerializer
 
@@ -6,5 +10,14 @@ from .serializers import ProductSerializer
 
 
 class ProductViewSet(ModelViewSet):
-    queryset = Product.objects.all()
+    queryset = Product.objects.all().order_by('updated')
     serializer_class = ProductSerializer
+
+
+    def destroy(self, request, pk):
+        queryset = get_object_or_404(Product, pk=pk)
+        try:
+            queryset.delete()
+            return Response({'پیام':'محصول حذف شد'}, status=status.HTTP_200_OK)
+        except ProtectedError:
+            return Response({'پیام':'این محصول را نمی توان حذف کرد زیرا به یک سبد خرید مرتبط است'}, status=status.HTTP_406_NOT_ACCEPTABLE)
