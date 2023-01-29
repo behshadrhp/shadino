5f50f279f0d355b254729cee525055f4f462c0a7
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-01-29 18:45:52 +0330

    update user field in like.models

--- like/models.py
+++ like/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
-from auth.models import User
+from django.conf import settings
 
 # Create your models here.
 
+
 class LikeItem(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='کاربر')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE, verbose_name='کاربر')
 
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.UUIDField()
