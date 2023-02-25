02518baa8c16bf5b28ab6f08c63a9c8ace587044
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-02-25 15:58:19 +0330

    conf: Media-URL & Media-ROOT

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,6 +10,8 @@ urlpatterns = [
     path('store/', include('store.urls')),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 admin.site.site_header = 'شادینو'
 admin.site.site_title = 'پنل مدیریتی فروشگاه شادینو'
