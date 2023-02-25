ea4a1554abce868e59bec61633a9bcd5210a8ae4
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-02-25 16:07:44 +0330

    create: ProductImage model

--- store/models.py
+++ store/models.py
@@ -38,6 +38,12 @@ class Product(models.Model):
         ordering = ['-updated']
 
 
+class ProductImage(models.Model):
+    product = models.ForeignKey(
+        'Product', on_delete=models.CASCADE, related_name='images', verbose_name='محصول')
+    image = models.ImageField(upload_to='store/product')
+
+
 class Customer(models.Model):
 
     # membership choice
