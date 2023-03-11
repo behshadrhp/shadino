52d752ef77ca4fe4b448e118ee4031960af98357
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-03-11 09:57:33 +0330

    conf: base celery app

--- core/celery.py
+++ core/celery.py
@@ -1,8 +1,10 @@
 import os
 from celery import Celery
 
+# local variable
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE',     'core.settings')
-celery = Celery('shadino')
-celery.config_from_object('django.conf:settings', namespace='CELERY')
-celery.autodiscover_tasks()
+# create Celery app
+app = Celery('core')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks() 
